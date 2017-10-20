6604ef546778dc8d0f78a2775d71f53a3cb4d990
Mescht <mescht93@gmail.com>
2017-10-20 19:48:18 +0200

    Help feature and resource cleanup

--- mobile/src/main/java/at/shockbytes/remote/fragment/AppsFragment.java
+++ mobile/src/main/java/at/shockbytes/remote/fragment/AppsFragment.java
@@ -2,7 +2,6 @@ package at.shockbytes.remote.fragment;
 
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.DividerItemDecoration;
@@ -87,12 +86,6 @@ public class AppsFragment extends BaseFragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_apps, container, false);
-    }
-
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        setupViews();
     }
 
     @Override
---
+++ mobile/src/main/java/at/shockbytes/remote/fragment/help/MainHelpFragment.java
@@ -0,0 +1,67 @@
+package at.shockbytes.remote.fragment.help;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import at.shockbytes.remote.R;
+import at.shockbytes.remote.fragment.BaseFragment;
+import butterknife.OnClick;
+
+/**
+ * @author Martin Macheiner
+ *         Date: 17.10.2017.
+ */
+
+public class MainHelpFragment extends BaseFragment {
+
+    public static MainHelpFragment newInstance() {
+        MainHelpFragment fragment = new MainHelpFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_help_main, container, false);
+    }
+
+    @Override
+    protected void setupViews() {
+    }
+
+    @OnClick(R.id.fragment_help_main_btn_apps)
+    protected void onClickApps() {
+        showFragment(GenericHelpFragment.newInstance(GenericHelpFragment.HelpType.APPS));
+    }
+
+    @OnClick(R.id.fragment_help_main_btn_mouse)
+    protected void onClickMouse() {
+        showFragment(MouseHelpFragment.newInstance());
+    }
+
+    @OnClick(R.id.fragment_help_main_btn_files)
+    protected void onClickFiles() {
+        showFragment(GenericHelpFragment.newInstance(GenericHelpFragment.HelpType.FILES));
+    }
+
+    @OnClick(R.id.fragment_help_main_btn_slides)
+    protected void onClickSlides() {
+        showFragment(GenericHelpFragment.newInstance(GenericHelpFragment.HelpType.SLIDES));
+    }
+
+    private void showFragment(Fragment fragment) {
+        getFragmentManager()
+                .beginTransaction()
+                .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out,
+                        android.R.anim.fade_in, android.R.anim.fade_out)
+                .replace(android.R.id.content, fragment)
+                .addToBackStack(null)
+                .commit();
+    }
+
+}
